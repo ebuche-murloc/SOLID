6813784b925d56376c5913de93f538193b4a6637
ebuche-murloc <immortalmagicdend@gmail.com>
2023-02-12 19:31:09 +0500

    Add project files.

---
+++ SingleResponsibility/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace SingleResponsibility;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        
+    }
+}
+
+class Journal
+{
+    private readonly List<string> entries = new List<string>();
+
+    public void AddEntry(string entry)
+    {
+        entries.Add(entry);
+    }
+    
+    public void RemoveEntry(int index)
+    {
+        if (index >= 0 && entries.Count < index)
+            entries.RemoveAt(index);
+    }
+
+    public override string ToString()
+    {
+        return string.Join(Environment.NewLine, entries);
+    }
+}
