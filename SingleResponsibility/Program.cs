cb0148c76cbce0d9e065f016f9e0819257c15ab4
ebuche-murloc <immortalmagicdend@gmail.com>
2023-02-12 19:47:46 +0500

    just journal functional

--- SingleResponsibility/Program.cs
+++ SingleResponsibility/Program.cs
@@ -4,19 +4,20 @@ class Program
 {
     public static void Main(string[] args)
     {
-        
+
     }
 }
 
+//Just journal
 class Journal
 {
     private readonly List<string> entries = new List<string>();
-
+    
     public void AddEntry(string entry)
     {
         entries.Add(entry);
     }
-    
+
     public void RemoveEntry(int index)
     {
         if (index >= 0 && entries.Count < index)
