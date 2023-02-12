b00d67f8ac9a236fc975c5621260f6580bb2ae18
ebuche-murloc <immortalmagicdend@gmail.com>
2023-02-12 20:40:20 +0500

    added persistence manager to separate logic of saving to file from journal

---
+++ SOLID/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics;
+using SOLID.SRP;
+
+namespace SOLID;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        SRPEntry.SRPMain(args);
+    }
+}
---
+++ SOLID/SRP/SingleResponcibilityPrinciple.cs
@@ -0,0 +1,68 @@
+﻿using System.Diagnostics;
+
+namespace SOLID.SRP;
+
+class SRPEntry
+{
+    public static void SRPMain(string[] args)
+    {
+        var journal = new Journal();
+        journal.AddEntry("Da eto josko bylo");
+        journal.AddEntry("Slabovato pogg");
+        Console.WriteLine(journal);
+
+        //better using dependency injection mb
+        var persistenceManager = new PersistenceManager();
+        var filename = @"Z:\j.txt";
+        persistenceManager.Save(journal, filename);
+
+        var psi = new ProcessStartInfo();
+        psi.FileName = filename;
+        psi.UseShellExecute = true;
+        Process.Start(psi);
+    }
+}
+
+class PersistenceManager
+{
+    public void Save(Journal journal, string filename, bool overwrite = false)
+    {
+        if (overwrite || !File.Exists(filename))
+            File.WriteAllText(filename, journal.ToString());
+    }
+}
+
+//Just journal
+class Journal
+{
+    private readonly List<string> entries = new List<string>();
+
+    public void AddEntry(string entry)
+    {
+        entries.Add(entry);
+    }
+
+    public void RemoveEntry(int index)
+    {
+        if (index >= 0 && entries.Count < index)
+            entries.RemoveAt(index);
+    }
+
+    public override string ToString()
+    {
+        return string.Join(Environment.NewLine, entries);
+    }
+
+    //adding file manipulation logic and breaking SR principle
+    public void Save(string filename, bool overwrite = false)
+    {
+        if (overwrite || !File.Exists(filename))
+            File.WriteAllText(filename, entries.ToString());
+    }
+
+    public void Load(string filename)
+    {
+        foreach (var s in File.ReadAllLines(filename))
+            entries.Add(s);
+    }
+}
